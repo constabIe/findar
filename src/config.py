04404d32f056fffc3b3c42225bc612eb0782fba0
Almir Avhadiev <almir.avhadiev@gmail.com>
2025-10-19 15:13:47 +0300

    fix: linting

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 from dynaconf import Dynaconf
 
-setttings = Dynaconf(
+settings = Dynaconf(
     envvar_prefix="DYNACONF",
     settings_files=["../settings.toml", "../.secrets.toml"],
 )
 
-# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`. `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`. `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
 # `settings_files` = Load these files in the order.
