88e3939cd371c8b1ffb0d6c518418acd3bf0c1e7
Almir Avhadiev <almir.avhadiev@gmail.com>
2025-10-19 15:17:56 +0300

    _

--- src/config.py
+++ src/config.py
@@ -5,5 +5,5 @@ settings = Dynaconf(
     settings_files=["../settings.toml", "../.secrets.toml"],
 )
 
-# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.`envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.`envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
 # `settings_files` = Load these files in the order.
