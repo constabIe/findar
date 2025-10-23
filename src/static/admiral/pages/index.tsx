850940093aee9c9752ab4287f3fe6eaf68e2b761
dilfd2006 <you@example.com>
2025-10-23 12:53:15 +0300

    fix: deleted unused modules, now only admiral pages are available

--- src/static/admiral/pages/index.tsx
+++ src/static/admiral/pages/index.tsx
@@ -3,16 +3,10 @@ import { Page, Card } from '@devfamily/admiral'
 
 const Home: React.FC = () => {
     return (
-        <Page title="Welcome">
+        <Page title="Welcome to the Findar project!">
             <Card>
                 <h2>Admin Panel</h2>
-                <p>Use the sidebar menu to navigate between sections:</p>
-                <ul>
-                    <li>Transactions</li>
-                    <li>Rules</li>
-                    <li>Profile</li>
-                    <li>Graphics</li>
-                </ul>
+                <p>Use the sidebar menu to navigate between sections</p>
             </Card>
         </Page>
     )
