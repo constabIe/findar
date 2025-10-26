824b4a75326961123360de26635764548024553a
Almir Avhadiev <almir.avhadiev@gmail.com>
2025-10-26 18:14:47 +0300

    _

--- src/static/admiral/pages/graphics/index.tsx
+++ src/static/admiral/pages/graphics/index.tsx
@@ -8,7 +8,7 @@ const Graphics: React.FC = () => {
         <h3>System Overview Dashboard</h3>
         <div style={{ width: "100%", height: "800px", border: "none" }}>
           <iframe
-            src="http://localhost:3014/public-dashboards/1d9ffa1547ee427a86205ebcc04718f2"
+            src="http://localhost:3000/grafana/public-dashboards/bb70f78eeba7467c879ecce7e89bcc7d"
             width="100%"
             height="100%"
             frameBorder="0"
